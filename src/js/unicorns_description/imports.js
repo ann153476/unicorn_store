d95153f9eca805b0248baa212e345ef216449e0c
Anka <103377387+ann153476@users.noreply.github.com>
2023-06-28 20:50:21 +0300

    all

---
+++ src/js/unicorns_description/imports.js
@@ -0,0 +1,176 @@
+import u1 from '../../../assets/img/unicornsimg/unicorn(1).png';
+import u2 from '../../../assets/img/unicornsimg/unicorn(2).png';
+import u3 from '../../../assets/img/unicornsimg/unicorn(3).png';
+import u4 from '../../../assets/img/unicornsimg/unicorn(4).png';
+import u5 from '../../../assets/img/unicornsimg/unicorn(5).png';
+import u6 from '../../../assets/img/unicornsimg/unicorn(6).png';
+import u7 from '../../../assets/img/unicornsimg/unicorn(7).png';
+import u8 from '../../../assets/img/unicornsimg/unicorn(8).png';
+import u9 from '../../../assets/img/unicornsimg/unicorn(9).png';
+import u10 from '../../../assets/img/unicornsimg/unicorn(10).png';
+import u11 from '../../../assets/img/unicornsimg/unicorn(11).png';
+import u12 from '../../../assets/img/unicornsimg/unicorn(12).png';
+import u13 from '../../../assets/img/unicornsimg/unicorn(13).png';
+import u14 from '../../../assets/img/unicornsimg/unicorn(14).png';
+import u15 from '../../../assets/img/unicornsimg/unicorn(15).png';
+import u16 from '../../../assets/img/unicornsimg/unicorn(16).png';
+import u17 from '../../../assets/img/unicornsimg/unicorn(17).png';
+import u18 from '../../../assets/img/unicornsimg/unicorn(18).png';
+import u19 from '../../../assets/img/unicornsimg/unicorn(19).png';
+import u20 from '../../../assets/img/unicornsimg/unicorn(20).png';
+import u21 from '../../../assets/img/unicornsimg/unicorn(21).png';
+import u22 from '../../../assets/img/unicornsimg/unicorn(22).png';
+import u23 from '../../../assets/img/unicornsimg/unicorn(23).png';
+import u24 from '../../../assets/img/unicornsimg/unicorn(24).png';
+import u25 from '../../../assets/img/unicornsimg/unicorn(25).png';
+import u26 from '../../../assets/img/unicornsimg/unicorn(26).png';
+import u27 from '../../../assets/img/unicornsimg/unicorn(27).png';
+import u28 from '../../../assets/img/unicornsimg/unicorn(28).png';
+import u29 from '../../../assets/img/unicornsimg/unicorn(29).png';
+import u30 from '../../../assets/img/unicornsimg/unicorn(30).png';
+import u31 from '../../../assets/img/unicornsimg/unicorn(31).png';
+import u32 from '../../../assets/img/unicornsimg/unicorn(32).png';
+import u33 from '../../../assets/img/unicornsimg/unicorn(33).png';
+import u34 from '../../../assets/img/unicornsimg/unicorn(34).png';
+import u35 from '../../../assets/img/unicornsimg/unicorn(35).png';
+import u36 from '../../../assets/img/unicornsimg/unicorn(36).png';
+import u37 from '../../../assets/img/unicornsimg/unicorn(37).png';
+import u38 from '../../../assets/img/unicornsimg/unicorn(38).png';
+import u39 from '../../../assets/img/unicornsimg/unicorn(39).png';
+import u40 from '../../../assets/img/unicornsimg/unicorn(40).png';
+import u41 from '../../../assets/img/unicornsimg/unicorn(41).png';
+import u42 from '../../../assets/img/unicornsimg/unicorn(42).png';
+import u43 from '../../../assets/img/unicornsimg/unicorn(43).png';
+import u44 from '../../../assets/img/unicornsimg/unicorn(44).png';
+import u45 from '../../../assets/img/unicornsimg/unicorn(45).png';
+import u46 from '../../../assets/img/unicornsimg/unicorn(46).png';
+import u47 from '../../../assets/img/unicornsimg/unicorn(47).png';
+import u48 from '../../../assets/img/unicornsimg/unicorn(48).png';
+import u49 from '../../../assets/img/unicornsimg/unicorn(49).png';
+import u50 from '../../../assets/img/unicornsimg/unicorn(50).png';
+import u51 from '../../../assets/img/unicornsimg/unicorn(51).png';
+import u52 from '../../../assets/img/unicornsimg/unicorn(52).png';
+import u53 from '../../../assets/img/unicornsimg/unicorn(53).png';
+import u54 from '../../../assets/img/unicornsimg/unicorn(54).png';
+import u55 from '../../../assets/img/unicornsimg/unicorn(55).png';
+import u56 from '../../../assets/img/unicornsimg/unicorn(56).png';
+import u57 from '../../../assets/img/unicornsimg/unicorn(57).png';
+import u58 from '../../../assets/img/unicornsimg/unicorn(58).png';
+import u59 from '../../../assets/img/unicornsimg/unicorn(59).png';
+import u60 from '../../../assets/img/unicornsimg/unicorn(60).png';
+import u61 from '../../../assets/img/unicornsimg/unicorn(61).png';
+import u62 from '../../../assets/img/unicornsimg/unicorn(62).png';
+import u63 from '../../../assets/img/unicornsimg/unicorn(63).png';
+import u64 from '../../../assets/img/unicornsimg/unicorn(64).png';
+import u65 from '../../../assets/img/unicornsimg/unicorn(65).png';
+import u66 from '../../../assets/img/unicornsimg/unicorn(66).png';
+import u67 from '../../../assets/img/unicornsimg/unicorn(67).png';
+import u68 from '../../../assets/img/unicornsimg/unicorn(68).png';
+import u69 from '../../../assets/img/unicornsimg/unicorn(69).png';
+import u70 from '../../../assets/img/unicornsimg/unicorn(70).png';
+import u71 from '../../../assets/img/unicornsimg/unicorn(71).png';
+import u72 from '../../../assets/img/unicornsimg/unicorn(72).png';
+import u73 from '../../../assets/img/unicornsimg/unicorn(73).png';
+import u74 from '../../../assets/img/unicornsimg/unicorn(74).png';
+import u75 from '../../../assets/img/unicornsimg/unicorn(75).png';
+import u76 from '../../../assets/img/unicornsimg/unicorn(76).png';
+import u77 from '../../../assets/img/unicornsimg/unicorn(77).png';
+import u78 from '../../../assets/img/unicornsimg/unicorn(78).png';
+import u79 from '../../../assets/img/unicornsimg/unicorn(79).png';
+import u80 from '../../../assets/img/unicornsimg/unicorn(80).png';
+import u81 from '../../../assets/img/unicornsimg/unicorn(81).png';
+import u82 from '../../../assets/img/unicornsimg/unicorn(82).png';
+import u83 from '../../../assets/img/unicornsimg/unicorn(83).png';
+import u84 from '../../../assets/img/unicornsimg/unicorn(84).png';
+import u85 from '../../../assets/img/unicornsimg/unicorn(85).png';
+import u86 from '../../../assets/img/unicornsimg/unicorn(86).png';
+
+export default arr = [
+  u1,
+  u2,
+  u3,
+  u4,
+  u5,
+  u6,
+  u7,
+  u8,
+  u9,
+  u10,
+  u11,
+  u12,
+  u13,
+  u14,
+  u15,
+  u16,
+  u17,
+  u18,
+  u19,
+  u20,
+  u21,
+  u22,
+  u23,
+  u24,
+  u25,
+  u26,
+  u27,
+  u28,
+  u29,
+  u30,
+  u31,
+  u32,
+  u33,
+  u34,
+  u35,
+  u36,
+  u37,
+  u38,
+  u39,
+  u40,
+  u41,
+  u42,
+  u43,
+  u44,
+  u45,
+  u46,
+  u47,
+  u48,
+  u49,
+  u50,
+  u51,
+  u52,
+  u53,
+  u54,
+  u55,
+  u56,
+  u57,
+  u58,
+  u59,
+  u60,
+  u61,
+  u62,
+  u63,
+  u64,
+  u65,
+  u66,
+  u67,
+  u68,
+  u69,
+  u70,
+  u71,
+  u72,
+  u73,
+  u74,
+  u75,
+  u76,
+  u77,
+  u78,
+  u79,
+  u80,
+  u81,
+  u82,
+  u83,
+  u84,
+  u85,
+  u86,
+];
+console.log(arr.length);
--- src/js/unicorns_description/unicorns_items.js
+++ src/js/unicorns_description/unicorns_items.js
@@ -1,12 +1,5 @@
 import { names_unicorns_ua, names_unicorns_en } from './names_unicorns.js';
-import u1 from '../../../assets/img/unicornsimg/unicorn(1).png';
-import u2 from '../../../assets/img/unicornsimg/unicorn(2).png';
-import u3 from '../../../assets/img/unicornsimg/unicorn(3).png';
-import u4 from '../../../assets/img/unicornsimg/unicorn(4).png';
-import u5 from '../../../assets/img/unicornsimg/unicorn(5).png';
-import u6 from '../../../assets/img/unicornsimg/unicorn(6).png';
-
-const arr = [u1, u2, u3, u4, u5, u6];
+import arr from './imports.js';
 let unicorns_items = [];
 for (let i = 0; i < names_unicorns_ua.length; i++) {
   var obj = {
@@ -19,5 +12,5 @@ for (let i = 0; i < names_unicorns_ua.length; i++) {
   };
   unicorns_items.push(obj);
 }
-console.log(unicorns_items);
+
 export default unicorns_items;
